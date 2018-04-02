9d8853ce4fc5a3b6bf7786fb207478c64ede4daa
Jorel Ali <jorel.k.ali@gmail.com>
2018-04-02 02:27:39 +0100

    Adds icon

--- source/gxsprites.c
+++ source/gxsprites.c
@@ -25,7 +25,8 @@
 #define DEFAULT_FIFO_SIZE	(256*1024)
 
 //Game speed constants
-#define PLAYER_X_AXIS_SPEED 2
+#define PLAYER_X_AXIS_SPEED 	2
+#define GRAVITY_CONSTANT		1
 
 static void *frameBuffer[2] = { NULL, NULL};
 
@@ -145,7 +146,7 @@ int main( int argc, char **argv ){
 	player.x = 320 << 8;	//center location
 	player.y = 240 << 8;	//center
 	player.dx = -256 * PLAYER_X_AXIS_SPEED;
-	player.dy = 256;
+	player.dy = 256 * GRAVITY_CONSTANT;
 	player.direction = 0;
 	
 	//Wii remote information
@@ -155,7 +156,13 @@ int main( int argc, char **argv ){
 	WPAD_GForce(0, &gforce); //get acceleration
 	
 	//Play music!
-	MP3Player_PlayBuffer(mystery_mp3, mystery_mp3_size, NULL);
+	//MP3Player_PlayBuffer(mystery_mp3, mystery_mp3_size, NULL);
+	
+	//Generate random platforms
+	//int i;
+	//for(i = 0; i < 5; i++) {
+	//	drawPlatform(rand() * 640, rand() * 480);
+	//}
 	
 	while(1) {
 
@@ -183,7 +190,9 @@ int main( int argc, char **argv ){
 		
 		//Player movement
 		player.x += (int) (player.dx * gforce.y);		//gforce.y is the left/right tilt of wiimote when horizontal (2 button to the right)
-		player.y += player.dy;							//generic vertical movement - gravity coming soon!
+		player.y += player.dy;
+		
+		player.dy += 128 * GRAVITY_CONSTANT;			//gravity
 		
 		//player direction changes when going left/right
 		if(gforce.y <= 0) {
@@ -199,7 +208,7 @@ int main( int argc, char **argv ){
 
 		//TODO gravity
 		if(player.y < (1<<8) || player.y > ((480-32) << 8))
-			player.dy = -player.dy;
+			player.dy = -264;
 		
 		drawDoodleJumper( player.x >> 8, player.y >> 8, player.direction);
 		
